fc0545920c96f551349d3daf0fd0708d08734a4f
unknown <nikola.antic@triplecrown.rs>
2018-07-27 16:29:50 +0200

    Sidebar - update expand function

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -11,23 +11,22 @@ const sidebar = (function () {
     let linkList = $$('.link-list');
     let globalSearch = $$('#global-search');
     let globalList = $$('.lists');
-    // variable to check sidebar, if isExpand = true sidebar is max size, else sidebar is collapsed
+    // variable to check sidebar, if isExpand = true sidebar is max size, else sidebar is collapsed, isExpandNav is like isExpand
     let isExpand = true;
+    let isExpandNav = true;
     window.addEventListener('load', () => {
         generateMenu();
     });
     expandButton.addEventListener('click', () => {
-        expand();
+        expand('sidebar');
     });
     back.addEventListener('click', () => {
-        navigation.classList.add('hide');
-        navigation.classList.remove('expand');
+        expand();
     });
     globalSearch.addEventListener('click', () => {
         chosenLink.innerHTML = 'Search';
         generateLink();
-        navigation.classList.add('expand');
-        navigation.classList.remove('hide');
+        expand();
     });
     function generateMenu() {
         let fragment = document.createDocumentFragment();
@@ -35,26 +34,39 @@ const sidebar = (function () {
             let tempFragment = document.createElement('div');
             tempFragment.innerHTML = `<div class="list-management" data-id="${arrayList[count]}"><span class="mdi mdi-magnify"></span><div class="list-name">${arrayList[count]}</div></div>`;
             tempFragment.childNodes[0].addEventListener('click', function () {
-                alert(listMenu[count].dataset.id);
+                // alert(listMenu[count].dataset.id);
                 chosenLink.dataset.id = listMenu[count].dataset.id;
                 chosenLink.innerHTML = listMenu[count].textContent;
                 generateLink(listMenu[count].dataset.id);
-                navigation.classList.add('expand');
-                navigation.classList.remove('hide');
+                expand();
             });
             fragment.appendChild(tempFragment.childNodes[0]);
         }
         linkWrapper.appendChild(fragment);
     }
-    function expand() {
-        if (isExpand) {
-            menu.classList.add('expand');
+    function expand(container) {
+        if (String(container) === 'sidebar') {
+            if (isExpand) {
+                menu.classList.add('expand');
+            }
+            else {
+                menu.classList.remove('expand');
+            }
+            isExpand = !isExpand;
         }
         else {
-            menu.classList.remove('expand');
+            if (isExpandNav) {
+                navigation.classList.add('show');
+                navigation.classList.remove('hide');
+            }
+            else {
+                navigation.classList.add('hide');
+                navigation.classList.remove('show');
+            }
+            isExpandNav = !isExpandNav;
         }
-        isExpand = !isExpand;
     }
+
     function generateLink(id) {
         listWrapper.innerHTML = '';
         let fragment = document.createDocumentFragment();
@@ -63,9 +75,8 @@ const sidebar = (function () {
                 let tempFragment = document.createElement('a');
                 tempFragment.innerHTML = `<a class="link-list" data-id="${data[id][count].id}">${data[id][count].name}</a>`;
                 tempFragment.childNodes[0].addEventListener('click', function () {
-                    alert(`ja imam id = ${linkList[count].dataset.id}, i trebam prikazati tabelu za ${linkList[count].textContent} i da vratim menu`);
-                    navigation.classList.add('hide');
-                    navigation.classList.remove('expand');
+                    //alert(`ja imam id = ${linkList[count].dataset.id}, i trebam prikazati tabelu za ${linkList[count].textContent} i da vratim menu`);
+                    expand();
                 });
                 fragment.appendChild(tempFragment.childNodes[0]);
             }
@@ -83,9 +94,8 @@ const sidebar = (function () {
                     let tempFragment = document.createElement('a');
                     tempFragment.innerHTML += `<a class="link-list" data-id="${count.id}">${count.name}</a>`;
                     tempFragment.childNodes[0].addEventListener('click', function () {
-                        alert(`ja imam id = ${linkList[list].dataset.id}, i trebam prikazati tabelu za ${linkList[list].textContent} i da vratim menu`);
-                        navigation.classList.add('hide');
-                        navigation.classList.remove('expand');
+                        //alert(`ja imam id = ${linkList[list].dataset.id}, i trebam prikazati tabelu za ${linkList[list].textContent} i da vratim menu`);
+                        expand();
                     });
                     fragment.appendChild(tempFragment.childNodes[0]);
                 }
