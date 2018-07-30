1d026c48b49875e8180b2b374e1bdfa41a2210b4
unknown <nikola.antic@triplecrown.rs>
2018-07-30 16:24:51 +0200

    Sidebar - tooltip -active list and link -defined buttons

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -3,54 +3,62 @@ const sidebar = (function () {
     let menu = $$('#sidebar');
     let linkWrapper = $$('#sidebar-link');
     let listMenu = $$('.list-management');
-    let expandButton = $$('#icon-expand');
+    let collapseButton = $$('#icon-collapse');
     let navigation = $$('#sidebar-local');
-    let back = $$('#back-button');
+    let back = $$('#back-to-menu');
     let chosenLink = $$('#chosen-link');
     let listWrapper = $$('#navigation-content');
     let linkList = $$('.link-list');
     let globalSearch = $$('#global-search');
     let globalList = $$('.lists');
-    // variable to check sidebar, if isExpand = true sidebar is max size, else sidebar is collapsed, isExpandNav is like isExpand
+    let listSelected = $$('.list-active');
+    let linkSelected = $$('.link-active');
+    // variable to check sidebar, if isExpand = true sidebar is max size, else sidebar is collapsed, isExpandNav is like iscollapse
     let isExpand = true;
     let isExpandNav = true;
     window.addEventListener('load', () => {
         generateMenu();
     });
-    expandButton.addEventListener('click', () => {
-        expand('sidebar');
+    collapseButton.addEventListener('click', () => {
+        collapse('sidebar');
     });
     back.addEventListener('click', () => {
-        expand();
+        collapse();
     });
     globalSearch.addEventListener('click', () => {
         chosenLink.innerHTML = 'Search';
         generateLink();
-        expand();
+        collapse();
     });
     function generateMenu() {
         let fragment = document.createDocumentFragment();
         for (let count in arrayList) {
             let tempFragment = document.createElement('div');
-            tempFragment.innerHTML = `<div class="list-management" data-id="${arrayList[count]}"><span class="mdi mdi-magnify"></span><div class="list-name">${arrayList[count]}</div></div>`;
+            tempFragment.innerHTML = `<div class="list-management tooltip" data-id="${arrayList[count]}">
+                <span class="mdi mdi-magnify icon-tooltip"></span>
+                <div class="list-name">${arrayList[count]}</div>
+                <span class="tooltip-text">${arrayList[count]}</span></div>`;
             tempFragment.childNodes[0].addEventListener('click', function () {
+                if (listSelected[0]) {
+                    listSelected[0].classList.remove('list-active');
+                }
                 // alert(listMenu[count].dataset.id);
-                chosenLink.dataset.id = listMenu[count].dataset.id;
-                chosenLink.innerHTML = listMenu[count].textContent;
+                listMenu[count].classList.add('list-active');
                 generateLink(listMenu[count].dataset.id);
-                expand();
+                chosenLink.dataset.id = listMenu[count].dataset.id;
+                chosenLink.innerHTML = chosenLink.dataset.id;
             });
             fragment.appendChild(tempFragment.childNodes[0]);
         }
         linkWrapper.appendChild(fragment);
     }
-    function expand(container) {
+    function collapse(container) {
         if (String(container) === 'sidebar') {
             if (isExpand) {
-                menu.classList.add('expand');
+                menu.classList.add('collapse');
             }
             else {
-                menu.classList.remove('expand');
+                menu.classList.remove('collapse');
             }
             isExpand = !isExpand;
         }
@@ -66,23 +74,35 @@ const sidebar = (function () {
             isExpandNav = !isExpandNav;
         }
     }
-
     function generateLink(id) {
-        listWrapper.innerHTML = '';
         let fragment = document.createDocumentFragment();
         if (id) {
-            for (let count in data[id]) {
-                let tempFragment = document.createElement('a');
-                tempFragment.innerHTML = `<a class="link-list" data-id="${data[id][count].id}">${data[id][count].name}</a>`;
-                tempFragment.childNodes[0].addEventListener('click', function () {
-                    //alert(`ja imam id = ${linkList[count].dataset.id}, i trebam prikazati tabelu za ${linkList[count].textContent} i da vratim menu`);
-                    expand();
-                });
-                fragment.appendChild(tempFragment.childNodes[0]);
+            if (id === chosenLink.dataset.id) {
+                collapse();
             }
-            listWrapper.appendChild(fragment);
+            else {
+                listWrapper.innerHTML = '';
+                for (let count in data[id]) {
+                    let tempFragment = document.createElement('a');
+                    tempFragment.innerHTML = `<a class="link-list" data-id="${data[id][count].id}">${data[id][count].name}</a>`;
+                    tempFragment.childNodes[0].addEventListener('click', function () {
+                        if (linkSelected[0]) {
+                            linkSelected[0].classList.remove('link-active');
+                        }
+                        linkList[count].classList.add('link-active');
+                        //alert(`ja imam id = ${linkList[count].dataset.id}, i trebam prikazati tabelu za ${linkList[count].textContent} i da vratim menu`);
+                        collapse();
+                    });
+                    fragment.appendChild(tempFragment.childNodes[0]);
+                }
+                listWrapper.appendChild(fragment);
+                collapse();
+
+            }
+
         }
         else {
+            listWrapper.innerHTML = '';
             for (let list of arrayList) {
                 let tempFragment = document.createElement('div');
                 tempFragment.innerHTML = `<div class="lists" data-id=${list}><h3>${list}</h3></div>`;
@@ -95,7 +115,7 @@ const sidebar = (function () {
                     tempFragment.innerHTML += `<a class="link-list" data-id="${count.id}">${count.name}</a>`;
                     tempFragment.childNodes[0].addEventListener('click', function () {
                         //alert(`ja imam id = ${linkList[list].dataset.id}, i trebam prikazati tabelu za ${linkList[list].textContent} i da vratim menu`);
-                        expand();
+                        collapse();
                     });
                     fragment.appendChild(tempFragment.childNodes[0]);
                 }
