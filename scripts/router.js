ac6be7482925f7489b4dd1a9107c31b936220ade
jovana-fe <jovana.mitic@backslash-soft.com>
2018-09-07 20:01:13 +0200

    Router module: showing and hiding HTML elements.

--- scripts/router.js
+++ scripts/router.js
@@ -1,13 +1,83 @@
 let router = (function(){
 
-   // window.addEventListener('popstate', function(e){console.log('url changed')});
+    function getElement(elementName) {
+        let element = document.getElementById(elementName);
+        return element;
+    }
 
-    // checkURLchange();
-    //
-    // window.onhashchange = funcRef;
+    function makePageActive(pageName) {
+        let pageElement = getElement(pageName);
+        pageElement.classList.add('active');
+    }
 
-    // window.onhashchange = function () {alert("nesto"); console.log("nesto isto");};
+    function hideActivePage(activePageName) {
+        let activeElement = getElement(activePageName);
+        activeElement.classList.remove('active');
+    }
 
+    function showPage(pageName) {
+        makePageActive(pageName);
+    }
+
+    function changePage(currentPageName, nextPageName) {
+        hideActivePage(currentPageName);
+        showPage(nextPageName);
+    }
+
+    showPage('page-machines');
+
+    changePage('page-machines', 'page-jackpot');
+
+    function bindNavigationLinkClickHandlers(className) {
+        //pokupiti sve na osnovu klase
+        //napraviti sta se desi kada se klikne
+    }
+
+    on('router-bind-navigation-click-handler',function() {
+        bindNavigationLinkClickHandlers();
+    });
+
+    on('router-change-page',function(e) {
+        var params = e.params();
+        changePage(e.page);
+        changePage('page-machines');
+    });
+
+})();
+
+
+/*
+const routesEnum = { "casino": "page-casino"};
+
+
+function bindNavigationLinkClickHandlers() {
+    //pokupis sve na osnovu klase
+    //napravis sta se desi kad kliknes
+}
+
+on('router-bind-navigation-click-handler',function() {
+    bindNavigationLinkClickHandlers();
+});
+on('router-change-page',function() {
+    changePage(activePage,params);
+});
+
+function changePage(activePage,params) {
+
+}
+*/
+
+/*window.addEventListener('popstate', function(e){console.log('url changed')});
+console.log("ulazi u funkciju");
+*/
+
+/*checkURLchange();
+
+window.onhashchange = funcRef;
+
+window.onhashchange = function () {alert("nesto"); console.log("nesto isto");};*/
+
+/*
     window.addEventListener("beforeunload", function (event) {
         // Most browsers.
         event.preventDefault();
@@ -15,10 +85,7 @@ let router = (function(){
         // Chrome/Chromium based browsers still need this one.
         event.returnValue = "\o/";
         console.log(event, location);
+        alert(window.location);
+
     });
-
-    //beforeunload ne mozemo da znamo na koju stranu ce da ide
-
-})();
-
-
+*/
