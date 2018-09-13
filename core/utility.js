fca6152cbee42b4ed782dac5b5ce051399cb398e
jovana-fe <jovana.mitic@backslash-soft.com>
2018-09-13 16:06:09 +0200

    Router module: Refreshing page

--- core/utility.js
+++ core/utility.js
@@ -9,6 +9,10 @@ function $$(selector) {
     }
 }
 
+//add i remove class
+
+
+
 const isAndroid = navigator.userAgent.toLowerCase().indexOf('android') > -1;
 const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
@@ -18,3 +22,4 @@ const isEdge = /Edge\/\d./i.test(navigator.userAgent);
 const isIe = !!navigator.userAgent.match(/Trident.*rv\:11\./);
 const isSafari = navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 const isOpera = !!window.opera || navigator.userAgent.indexOf('Opera') >= 0;
+
