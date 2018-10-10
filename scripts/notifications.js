debe3c9abfc7fd607cc569c5b2e89ff8a3a255f6
jovana-fe <jovana.mitic@backslash-soft.com>
2018-10-10 14:45:10 +0200

    Notification module: - Notification types updated

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,10 +1,10 @@
 let notifications = function () {
 
     const messageTypes = {
-        ok: '0',
-        warning: '1',
-        info: '2',
-        error: '3'
+        ok: 0,
+        warning: 1,
+        info: 2,
+        error: 3
     };
 
     const messageCodes = {
@@ -12,28 +12,27 @@ let notifications = function () {
         23: 'Logout error!'
     };
 
-    console.log(messageCodes["22"]);
-
     function showNotification(params) {
         let newElement = params.element;
         let messageType = params.model.type;
+        console.log(messageType);
         if (messageType === messageTypes.ok) {
-            newElement.classList.add('notification-success');
             newElement.classList.add('toast-success');
         } else if (messageType ===  messageTypes.warning || messageType === messageTypes.error) {
-            newElement.classList.add('notification-error');
             newElement.classList.add('toast-error');
-        } else (messageType === messageTypes.info)
-        {
-            newElement.classList.add('notification-info');
+        } else if (messageType === messageTypes.info) {
             newElement.classList.add('toast-warning');
         }
         $$('.notifications-container')[0].appendChild(newElement);
     }
 
-
     on('notifications/render/finished', function (params) {
         showNotification(params);
+        $$('.btn-clear')[0].addEventListener('click', function(){
+            console.log($$('.notifications-container')[0]);
+            console.log($$('.notifications-container')[0].classList);
+            $$('.notifications-container')[0].classList.add('hidden');
+        });
     });
 
     on('notifications/show', function (params) {
