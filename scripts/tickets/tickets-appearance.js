592b44fc355a3d47b54b7d4ae57c3f07f627f3c4
unknown <nikola.antic@triplecrown.rs>
2018-10-17 10:13:48 +0200

    ticket: change picture, canvas elements

--- scripts/tickets/tickets-appearance.js
+++ scripts/tickets/tickets-appearance.js
@@ -19,12 +19,13 @@ const ticketAppearance = (function () {
     let chasableTicket = $$('#wrapper-tickets-appearance-cashable').children[0];
     let promoTicket = $$('#wrapper-tickets-appearance-promo').children[0];
     let inputPromoTicket = $$('#wrapper-tickets-appearance-promo').children[1].children[1];
-    let codeNumber = '# 00000000';
-    let codeTicket = '# 1';
-    let codeValueTxt = 'ONE HUNDRED RSD 0/100';
-    let codeCurrencyNumber = '100.00';
-    let codeDate;
-    let codeTime;
+    let dd = '16';
+    let MM = '10';
+    let yyyy = '2018';
+    let hh = '11';
+    let HH = '23';
+    let min = '43';
+    let sec = '03';
 
     let dateFormatArray = ['dd.MM.yyyy', 'dd/MM/yyyy', 'yyyy-MM-dd', 'dd-MM-yyyy', 'MM/dd/yyyy'];
     let timeFormatArray = ['hh:mm', 'hh:mm:ss', 'HH:mm', 'HH:mm:ss'];
@@ -40,12 +41,12 @@ const ticketAppearance = (function () {
         chasableTicket.classList.add('tab-active');
         draw(ticketNameCoordinate, inputChasoutTicket.value);
         if (isNaN(inputExpiringCashout.value)) {
-            draw(ticketVoidAfterNumber, inputExpiringCashout.value);
-            draw(ticketVoidAfterDays, '');
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringCashout.value);
+            draw(ticketVoidAfterDaysCoordinate, '');
         }
         else {
-            draw(ticketVoidAfterNumber, inputExpiringCashout.value);
-            draw(ticketVoidAfterDays, inputTicketVoidDays.value);
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringCashout.value);
+            draw(ticketVoidAfterDaysCoordinate, inputTicketVoidDays.value);
         }
     });
 
@@ -54,58 +55,14 @@ const ticketAppearance = (function () {
         promoTicket.classList.add('tab-active');
         draw(ticketNameCoordinate, inputPromoTicket.value);
         if (isNaN(inputExpiringPromo.value)) {
-            draw(ticketVoidAfterNumber, inputExpiringPromo.value);
-            draw(ticketVoidAfterDays, '');
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringPromo.value);
+            draw(ticketVoidAfterDaysCoordinate, '');
         }
         else {
-            draw(ticketVoidAfterNumber, inputExpiringPromo.value);
-            draw(ticketVoidAfterDays, inputTicketVoidDays.value);
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringPromo.value);
+            draw(ticketVoidAfterDaysCoordinate, inputTicketVoidDays.value);
         }
     });
-
-    function formatDate(format) {
-        switch (JSON.parse(format)) {
-
-            case dateFormatArray[1]:
-                codeDate = '16/10/2018';
-                break;
-
-            case dateFormatArray[2]:
-                codeDate = '2018-10-16';
-                break;
-
-            case dateFormatArray[3]:
-                codeDate = '16-10-2018'
-                break;
-
-            case dateFormatArray[4]:
-                codeDate = '10/16/2018';
-                break;
-
-            default:
-                codeDate = '16.10.2018';
-        }
-    }
-
-    function formatTime(format) {
-        switch (JSON.parse(format)) {
-
-            case timeFormatArray[1]:
-                codeTime = '11:02:23';
-                break;
-
-            case timeFormatArray[2]:
-                codeTime = '23:02';
-                break;
-
-            case timeFormatArray[3]:
-                codeTime = '23:02:23'
-                break;
-
-            default:
-                codeTime = '11:02';
-        }
-    }
 
     let casinoCoorinate = {
         x: 200,
@@ -129,120 +86,90 @@ const ticketAppearance = (function () {
         x: 200,
         y: 60,
         w: 400,
-        h: 60
+        h: 50
     }
     let validationCoordinate = {
         x: 200,
-        y: 200,
+        y: 167,
         w: 200,
-        h: 20
-    }
-    let validationNumberCoordinate = {
-        x: 400,
-        y: 200,
-        w: 200,
-        h: 20
+        h: 25
     }
     let dateCoordinate = {
         x: 200,
-        y: 220,
+        y: 191,
         w: 100,
-        h: 20
+        h: 25
     }
     let timeCoordinate = {
         x: 300,
-        y: 220,
+        y: 191,
         w: 100,
-        h: 20
+        h: 25
     }
     let ticketCoordinate = {
         x: 400,
-        y: 220,
+        y: 191,
         w: 100,
-        h: 20
-    }
-    let ticketNumberCoordinate = {
-        x: 500,
-        y: 220,
-        w: 100,
-        h: 20
-    }
-    let valueTxtCoordinate = {
-        x: 200,
-        y: 240,
-        w: 400,
-        h: 20
+        h: 25
     }
     let currencyCoordinate = {
         x: 200,
-        y: 260,
-        w: 200,
-        h: 40
-    }
-    let currencyNumberCoordinate = {
-        x: 400,
-        y: 260,
+        y: 240,
         w: 200,
         h: 40
     }
     let ticketVoidAfterCoordinate = {
-        x: 200,
-        y: 300,
-        w: 150,
-        h: 20
+        x: 150,
+        y: 290,
+        w: 200,
+        h: 30
     }
-    let ticketVoidAfterNumber = {
+    let ticketVoidAfterNumberCoordinate = {
         x: 350,
-        y: 300,
-        w: 50,
-        h: 20
-    }
-    let ticketVoidAfterDays = {
-        x: 400,
-        y: 300,
+        y: 290,
         w: 70,
-        h: 20
+        h: 30
+    }
+    let ticketVoidAfterDaysCoordinate = {
+        x: 420,
+        y: 290,
+        w: 70,
+        h: 30
     }
     let assetCoordinate = {
-        x: 470,
-        y: 300,
-        w: 80,
-        h: 20
+        x: 490,
+        y: 290,
+        w: 100,
+        h: 30
     }
-    let assetCoordinateNumber = {
-        x: 550,
-        y: 300,
+    let assetCoordinateNumberCoordinate = {
+        x: 590,
+        y: 290,
         w: 50,
-        h: 20
+        h: 30
     }
 
     window.addEventListener('load', function () {
-        drawBarcode();
-        formatDate(selectedDateFormat.dataset.items);
-        formatTime(selectedTimeFormat.dataset.items);
+        drawPicture();
         draw(casinoCoorinate, inputCasino.value);
         draw(addressCoordinate, inputAddress.value);
         draw(cityCoordinate, inputCity.value);
         draw(ticketNameCoordinate, inputChasoutTicket.value);
         draw(validationCoordinate, inputValidation.value);
-        draw(validationNumberCoordinate, codeNumber);
-        draw(dateCoordinate, codeDate);
-        draw(timeCoordinate, codeTime);
-        draw(ticketCoordinate, inputTicket.value)
-        draw(ticketNumberCoordinate, codeTicket);
-        draw(valueTxtCoordinate, codeValueTxt);
+        draw(dateCoordinate, JSON.parse(selectedDateFormat.dataset.items).replace('dd', dd).replace('MM', MM).replace('yyyy', yyyy));
+        draw(timeCoordinate, JSON.parse(selectedTimeFormat.dataset.items).replace('hh', hh).replace('HH', HH).replace('mm', min).replace('ss', sec));
+        draw(ticketCoordinate, `${inputTicket.value} #`)
         draw(currencyCoordinate, inputCurrency.value);
-        draw(currencyNumberCoordinate, codeCurrencyNumber);
         draw(ticketVoidAfterCoordinate, inputTicketVoid.value);
-        draw(ticketVoidAfterNumber, inputExpiringCashout.value);
+        draw(ticketVoidAfterNumberCoordinate, inputExpiringCashout.value);
         if (!isNaN(inputExpiringCashout.value)) {
-            draw(ticketVoidAfterDays, inputCurrency.value);
+            draw(ticketVoidAfterDaysCoordinate, inputCurrency.value);
         }
         else {
-            draw(ticketVoidAfterDays, '');
+            draw(ticketVoidAfterDaysCoordinate, '');
         }
-        draw(assetCoordinate, inputAsset.value);
-        draw(assetCoordinateNumber, `# ${inputAssetNumber.value}`);
+        draw(assetCoordinate, `${inputAsset.value} #`);
+        draw(assetCoordinateNumberCoordinate, inputAssetNumber.value);
     });
 
     ticketAppearanceAdvance.addEventListener('click', function () {
@@ -269,8 +196,8 @@ const ticketAppearance = (function () {
 
     inputExpiringCashout.addEventListener('keyup', function (event) {
         if (chasableTicket.classList.contains('tab-active')) {
-            draw(ticketVoidAfterNumber, inputExpiringCashout.value);
-            isNaN(inputExpiringCashout.value) ? draw(ticketVoidAfterDays, '') : draw(ticketVoidAfterDays, inputExpiringCashout.value);
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringCashout.value);
+            isNaN(inputExpiringCashout.value) ? draw(ticketVoidAfterDaysCoordinate, '') : draw(ticketVoidAfterDaysCoordinate, inputExpiringCashout.value);
         }
     });
 
@@ -282,8 +209,8 @@ const ticketAppearance = (function () {
 
     inputExpiringPromo.addEventListener('keyup', function (event) {
         if (promoTicket.classList.contains('tab-active')) {
-            draw(ticketVoidAfterNumber, inputExpiringPromo.value);
-            isNaN(inputExpiringPromo.value) ? draw(ticketVoidAfterDays, '') : draw(ticketVoidAfterDays, inputTicketVoidDays.value);
+            draw(ticketVoidAfterNumberCoordinate, inputExpiringPromo.value);
+            isNaN(inputExpiringPromo.value) ? draw(ticketVoidAfterDaysCoordinate, '') : draw(ticketVoidAfterDaysCoordinate, inputTicketVoidDays.value);
         }
     });
 
@@ -292,13 +219,11 @@ const ticketAppearance = (function () {
     });
 
     setDateFormat.addEventListener('click', function () {
-        formatDate(selectedDateFormat.dataset.items);
-        draw(dateCoordinate, codeDate);
+        draw(dateCoordinate, JSON.parse(selectedDateFormat.dataset.items).replace('dd', dd).replace('MM', MM).replace('yyyy', yyyy));
     });
 
     setTimeFormat.addEventListener('click', function () {
-        formatTime(selectedTimeFormat.dataset.items);
-        draw(timeCoordinate, codeTime);
+        draw(timeCoordinate, JSON.parse(selectedTimeFormat.dataset.items).replace('hh', hh).replace('HH', HH).replace('mm', min).replace('ss', sec));
     });
 
     inputTicket.addEventListener('keyup', function (event) {
@@ -327,7 +252,7 @@ const ticketAppearance = (function () {
     });
 
     inputAssetNumber.addEventListener('keyup', function (event) {
-        draw(assetCoordinateNumber, `# ${inputAssetNumber.value}`);
+        draw(assetCoordinateNumberCoordinate, `# ${inputAssetNumber.value}`);
     });
 
     function draw(coordinate, value) {
@@ -338,10 +263,11 @@ const ticketAppearance = (function () {
         ctx.font = `${coordinate.h - 10}px Arial`;
         ctx.fillText(txt, coordinate.x + coordinate.w / 2 - (ctx.measureText(txt).width / 2), coordinate.y + coordinate.h - 10);
     }
-    function drawBarcode() {
+    
+    function drawPicture() {
         let canvas = $$('#wrapper-canvas').children[0];
         let ctx = canvas.getContext("2d");
-        let img = document.getElementById("barcode");
-        ctx.drawImage(img, 200, 120, 400, 80);
+        let img = document.getElementById("ticket");
+        ctx.drawImage(img, 0, 0, 775, 325);
     }
 })();
