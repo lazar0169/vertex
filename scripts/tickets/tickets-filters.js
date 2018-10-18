5bc1c8b2be9512689a155da04370b089cab8ee85
anta988 <39047215+anta988@users.noreply.github.com>
2018-10-18 15:18:16 +0200

    Feature tickets (#20)
    * tickets: initial commit, added classes for filter elements
    
    * tickets: initial ticket appearance
    
    * tikets: convas elements
    
    * ticket: convas elements
    
    * ticket: canvas elements
    
    * tickets: canvas removed drawProba()
    
    * ticket: change picture, canvas elements
    
    * ticket: bug resolved
    
    * ticket: canvas side elements, barcode picture
    
    * ticktets: corrected side
    
    * tickets: canvas  elements and their values

---
+++ scripts/tickets/tickets-filters.js
@@ -0,0 +1,17 @@
+const ticketsFilter = (function () {
+    let ticketAdvanceFilter = $$('#tickets-advance-table-filter');
+    let ticketMachinesNumbers = $$('#tickets-machines-number');
+
+
+    ticketMachinesNumbers.appendChild(dropdown.generate(machinesNumber));
+
+
+    ticketAdvanceFilter.addEventListener('click', function () {
+        showAdvanceTableFilter()
+    });
+   
+    function showAdvanceTableFilter() {
+        ticketAdvanceFilter.classList.toggle('aft-advance-active');
+        
+    }
+})();
