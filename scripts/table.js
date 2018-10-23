3eeb42885735e63f37b34c7f04542e370740d9d6
jovana-fe <jovana.mitic@backslash-soft.com>
2018-10-23 13:35:02 +0200

    - Table module added - CSS for tables added - mapper.json updated - Home module updated to generate table

--- scripts/table.js
+++ scripts/table.js
@@ -1,4 +1,4 @@
-let home = (function () {
+let table = (function () {
 
     let testTable = [
         {
@@ -173,15 +173,61 @@ let home = (function () {
         }
     ];
 
-    on('home/activated', function () {
-        setTimeout(function () {
-            trigger('preloader/hide');
-        }, 2000);
+    let rows = [];
 
-        let tableVar = table.generate(testTable);
-        console.log(tableVar);
+    function generate(json, id = '', sticky = false) {
+        console.log('pozvali smo table generate');
 
-        $$('#page-home').appendChild(tableVar);
-    });
+        let colsCount = Object.keys(json[0]).length;
+        let tbody = document.createElement('div');
+        tbody.style.gridTemplateColumns = `repeat(${colsCount}, 1fr)`;
+        tbody.style.gridTemplateRows = `repeat(${json.length}, 1fr)`;
+        tbody.id = id;
+        tbody.className = 'tbody';
+
+        for (let col = 0; col < colsCount; col++) {
+            let head = document.createElement('div');
+            head.innerHTML = Object.keys(json[0])[col];
+            head.className = 'head cell';
+            tbody.appendChild(head);
+        }
+        for (let row = 0; row < json.length; row++) {
+            let rowId = Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+            while (rows.includes(rowId)) {
+                rowId = Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+            }
+            rows.push(rowId);
+            for (let col = 0; col < colsCount; col++) {
+                let cell = document.createElement('div');
+                cell.innerHTML = json[row][Object.keys(json[row])[col]];
+                cell.className = col === 0 ? 'first cell' : 'cell';
+                cell.classList.add(`row-${rowId}`);
+                cell.addEventListener('mouseover', function () {
+                    hoverRow(`row-${rowId}`, true);
+                }, {passive: false});
+                cell.addEventListener('mouseout', function () {
+                    hoverRow(`row-${rowId}`, false);
+                }, {passive: false});
+                tbody.appendChild(cell);
+            }
+        }
+
+        function hoverRow(elements, highlight = false) {
+            for (let element of document.getElementsByClassName(elements)) {
+                element.classList[highlight ? "add" : "remove"]('hover');
+            }
+        }
+
+        let t = document.createElement('div');
+        t.className = sticky ? 'table sticky' : 'table';
+        t.appendChild(tbody);
+        console.log('t', t);
+        return t;
+    }
+
+    return {
+        generate: generate
+    };
+
 
 })();
