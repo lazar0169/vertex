c82b4d45fe8f6bceddbf9f8355e42195df10f5e6
unknown <nikola.antic@triplecrown.rs>
2018-10-31 10:33:37 +0100

    top-bar:  username from token

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -4,6 +4,9 @@ const userEditProfileModule = (function () {
     let userEditProfile = $$('#user-edit-profile');
     let backUserEditProfile = $$('#user-edit-profile-buttons').children[0];
     let saveUserEditProfile = $$('#user-edit-profile-buttons').children[1];
+    let usernameDiv = $$('#user-info-profile-username');
+
+    usernameDiv.innerHTML = $$('#top-bar-logout-user').innerHTML;
 
     editYourProfile.addEventListener('click', function () {
         userEditProfile.classList.remove('hidden');
@@ -15,7 +18,7 @@ const userEditProfileModule = (function () {
     backUserEditProfile.addEventListener('click', function () {
         trigger('show/app');
     });
-    saveUserEditProfile.addEventListener('click', function(){
+    saveUserEditProfile.addEventListener('click', function () {
         alert('Save changes');
         trigger('show/app');
     });
