801e4c31606a05a427da8fcda3b293ff533da932
unknown <nikola.antic@triplecrown.rs>
2018-11-30 13:59:53 +0100

    jackpot: background open add-new-jackpot-settings

--- scripts/jackpot/add-new-jackpot.js
+++ scripts/jackpot/add-new-jackpot.js
@@ -19,9 +19,13 @@ const addNewJackpot = (function () {
                 for (let element of addNewJackpotControlSettingsInfo) {
                     if (control !== element) {
                         element.parentNode.parentNode.children[1].classList.add('hidden');
+                        element.parentNode.classList.remove('expanded-add-new-jackpot-settings');
+
                     }
                     else {
                         element.parentNode.parentNode.children[1].classList.toggle('hidden');
+                        element.parentNode.classList.toggle('expanded-add-new-jackpot-settings');
+
                     }
                 }
             });
@@ -33,17 +37,20 @@ const addNewJackpot = (function () {
                     checkSwitch.children[0].children[0].children[0].children[0].checked = false;
                     checkSwitch.children[0].children[1].children[1].innerHTML = 'Off';
                     checkSwitch.children[1].classList.add('hidden');
+                    checkSwitch.children[0].classList.remove('expanded-add-new-jackpot-settings');
 
                 }
                 else {
                     checkSwitch.children[0].children[0].children[0].children[0].checked = true;
                     checkSwitch.children[0].children[1].children[1].innerHTML = 'On';
                     checkSwitch.children[1].classList.remove('hidden');
+                    checkSwitch.children[0].classList.add('expanded-add-new-jackpot-settings');
 
                 }
                 for (let currentCheck of addNewJackpotControlSettings) {
-                    if (checkSwitch.children[0].children[0].children[0] !== currentCheck.children[0].children[0].children[0]) {
+                    if (checkSwitch.children[0].children[0].children[0] !== currentCheck.children[0].children[0].children[0] && checkSwitch.children[0].children[0].children[0].children[0].checked) {
                         currentCheck.children[1].classList.add('hidden');
+                        currentCheck.children[0].classList.remove('expanded-add-new-jackpot-settings');
                     }
                 }
                 checkSwitch.children[0].classList.toggle('checked-add-new-jackpot-settings');
