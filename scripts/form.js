28e8aa7438490b255df2519496287acb4cf9180c
jovana-fe <jovana.mitic@backslash-soft.com>
2018-12-05 16:38:50 +0100

    Form module: - Form module added

---
+++ scripts/form.js
@@ -0,0 +1,42 @@
+let form = (function(){
+
+    function init(formSettings) {
+
+    }
+
+
+    function update(formSettings) {
+
+    }
+
+
+    function validate(formSettings) {
+
+    }
+
+
+    function error(formSettings) {
+
+    }
+
+
+    function success(formSettings) {
+
+    }
+
+
+    function fillData(formSettings) {
+
+    }
+
+
+    function serialize(formSettings) {
+
+    }
+
+
+    function submit(formSettings) {
+
+    }
+
+})();
