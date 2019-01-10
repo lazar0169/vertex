ee820ec0899d801a5053fa95a823b4234d726bf7
unknown <nikola.antic@triplecrown.rs>
2019-01-10 16:17:57 +0100

    users: add-new-user page

--- scripts/users/users.js
+++ scripts/users/users.js
@@ -2,14 +2,23 @@ const users = (function () {
     let addNewUserButtonWrapper = $$('#users-add-new-user-button');
     let usersNumber = $$('#users-number');
     let addNewUserWrapper = $$('#users-add-new-user');
+    let addNewUserButtonsWrapper = $$('#add-new-user-buttons-wrapper');
     trigger('preloader/hide');
 
     usersNumber.appendChild(dropdown.generate(machinesNumber));
 
     addNewUserButtonWrapper.children[0].addEventListener('click', function () {
         // alert('otvori formu za dodavanje novog korisnika');
-        addNewUserButtonWrapper.children[0].classList.toggle('add-new-user-active');
-        addNewUserWrapper.classList.toggle('hidden');
+        addNewUserButtonWrapper.children[0].classList.add('add-new-user-active');
+        addNewUserWrapper.classList.remove('hidden');
 
     });
+
+    addNewUserButtonsWrapper.children[1].addEventListener('click', function(){
+        addNewUserButtonWrapper.children[0].classList.remove('add-new-user-active');
+        addNewUserWrapper.classList.add('hidden');
+    });
+
+    
+
 })();
