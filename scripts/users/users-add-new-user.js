b168872aa35032d82ad31c78cc71041ca6988bb4
unknown <nikola.antic@triplecrown.rs>
2019-01-14 12:26:22 +0100

    users: set click event on radio and checkbox

--- scripts/users/users-add-new-user.js
+++ scripts/users/users-add-new-user.js
@@ -21,9 +21,9 @@ const usersAddNewUser = (function () {
                 checkedSwitchMode = 'Off';
             }
             roleWrapper.innerHTML = `<div class="form-section-content" >
-                <figure class="element-form-check vertex-form-checkbox" data-target="${roleData.RoleList[count].MainRoleName}-role">
+                <figure class="element-form-check vertex-form-checkbox add-new-user-checked" data-target="${roleData.RoleList[count].MainRoleName}-role">
                     <label class="form-switch">
-                        <input name="${roleData.RoleList[count].MainRoleName}-role" data-type="int" class="element-form-data" type="checkbox" ${checkedSwitch}>
+                        <input name="${roleData.RoleList[count].MainRoleName}-role" data-type="int" class="element-form-data add-new-user-checked-status" type="checkbox" ${checkedSwitch}>
                         <i class="form-icon"></i>
                     </label>
                     <div id="${roleData.RoleList[count].MainRoleName}-role-enable" class="element-form-mode checkbox-label element-dynamic-translatable" data-translation-key="${roleData.RoleList[count].MainRoleName}"></div>
@@ -53,19 +53,47 @@ const usersAddNewUser = (function () {
                     checkedBox = '';
 
                 }
-                categoryRoleWrapper.innerHTML = `<figure class="element-form-check vertex-form-checkbox-box">
+                categoryRoleWrapper.innerHTML = `<figure class="element-form-check vertex-form-checkbox-box add-new-user-checked">
                     <label class="form-checkbox" >
-                        <input type="checkbox" ${checkedBox}>
+                        <input type="checkbox" class="add-new-user-checked-status" ${checkedBox}>
                         <i class="form-icon" data-elementId = "${roleData.RoleList[count].MainRoleList[nesto].Name}"></i> <div data-translation-key="${roleData.RoleList[count].MainRoleList[nesto].Name}">${roleData.RoleList[count].MainRoleList[nesto].Name}</div>
                     </label>
                 </figure>`
                 addNewUserRoleList[count].appendChild(categoryRoleWrapper);
             }
         }
+    }
 
+
+    function setClickAndChangeCheckedStatus() {
+        let checkbox = $$('.add-new-user-checked');
+
+        for (let element of checkbox) {
+
+            switch (element.getElementsByClassName('add-new-user-checked-status')[0].type) {
+
+                case 'radio':
+                    element.addEventListener('click', function () {
+                        element.getElementsByClassName('add-new-user-checked-status')[0].checked = true;
+                    })
+                    break;
+
+                case 'checkbox':
+                    element.addEventListener('click', function (e) {
+                        if (element.getElementsByClassName('add-new-user-checked-status')[0].checked) {
+                            element.getElementsByClassName('add-new-user-checked-status')[0].checked = false;
+                        }
+                        else {
+                            element.getElementsByClassName('add-new-user-checked-status')[0].checked = true;
+                        }
+                    })
+                    break;
+            }
+        }
     }
 
     on('users/generate-role', function (data) {
-        generateRole(data.role)
+        generateRole(data.role);
+        setClickAndChangeCheckedStatus();
     })
 })();
