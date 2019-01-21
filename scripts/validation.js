9642ba450eb0529d6492fc75dbf09bd8f5794786
Nenad Milosavljevic <nenadmilosavljevic89@gmail.com>
2019-01-21 01:14:50 +0100

    validation started

---
+++ scripts/validation.js
@@ -0,0 +1,40 @@
+let localization = (function () {
+
+    const inputTypes = {
+        singleSelect : 'single-select',
+        integer: 'int',
+        float:'float',
+        string:'string',
+        array:'array'
+    }
+
+
+    function init() {
+
+    }
+
+    function bindHandlers() {
+
+    }
+
+    function validate(){
+
+    }
+
+    function showErrors(){
+
+    }
+
+    function hideErrors() {
+
+    }
+
+    function setHardRules () {
+
+    }
+
+    function setDefaultSettings() {
+        
+    }
+
+})();
