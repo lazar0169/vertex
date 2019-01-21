c7a377394dc8c33ad30ec314ef502b8d82be91d8
unknown <nikola.antic@triplecrown.rs>
2019-01-21 12:07:02 +0100

    add-new-jackpot: resolved arrows

--- scripts/jackpot/add-new-jackpot.js
+++ scripts/jackpot/add-new-jackpot.js
@@ -20,11 +20,13 @@ const addNewJackpot = (function () {
                     if (control !== element) {
                         element.parentNode.parentNode.children[1].classList.add('hidden');
                         element.parentNode.classList.remove('expanded-add-new-jackpot-settings');
+                        element.children[1].classList.remove('opened-arrow');
 
                     }
                     else {
                         element.parentNode.parentNode.children[1].classList.toggle('hidden');
                         element.parentNode.classList.toggle('expanded-add-new-jackpot-settings');
+                        element.children[1].classList.toggle('opened-arrow');
 
                     }
                 }
@@ -35,26 +37,28 @@ const addNewJackpot = (function () {
                 e.preventDefault();
                 if (checkSwitch.children[0].children[0].children[0].children[0].checked) {
                     checkSwitch.children[0].children[0].children[0].children[0].checked = false;
-                    checkSwitch.children[0].children[1].children[1].innerHTML = 'Off';
+                    checkSwitch.children[0].children[1].children[0].children[1].innerHTML = 'Off';
                     checkSwitch.children[1].classList.add('hidden');
                     checkSwitch.children[0].classList.remove('expanded-add-new-jackpot-settings');
+                    checkSwitch.children[0].children[1].children[1].classList.remove('opened-arrow');
 
                 }
                 else {
                     checkSwitch.children[0].children[0].children[0].children[0].checked = true;
-                    checkSwitch.children[0].children[1].children[1].innerHTML = 'On';
+                    checkSwitch.children[0].children[1].children[0].children[1].innerHTML = 'On';
                     checkSwitch.children[1].classList.remove('hidden');
                     checkSwitch.children[0].classList.add('expanded-add-new-jackpot-settings');
-
+                    checkSwitch.children[0].children[1].children[1].classList.add('opened-arrow');
                 }
                 for (let currentCheck of addNewJackpotControlSettings) {
                     if (checkSwitch.children[0].children[0].children[0] !== currentCheck.children[0].children[0].children[0] && checkSwitch.children[0].children[0].children[0].children[0].checked) {
                         currentCheck.children[1].classList.add('hidden');
                         currentCheck.children[0].classList.remove('expanded-add-new-jackpot-settings');
+                        currentCheck.children[0].children[1].children[1].classList.remove('opened-arrow');
                     }
                 }
                 checkSwitch.children[0].classList.toggle('checked-add-new-jackpot-settings');
-                checkSwitch.children[0].children[1].children[1].classList.toggle('active-add-new-jackpot-settings')
+                checkSwitch.children[0].children[1].children[0].children[1].classList.toggle('active-add-new-jackpot-settings')
             })
         }
     });
