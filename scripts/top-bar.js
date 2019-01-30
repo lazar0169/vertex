865b6267914a3f9690da608e9ace6c3b35de357d
unknown <nikola.antic@triplecrown.rs>
2019-01-30 12:11:36 +0100

    update-logout: initial commit

--- scripts/top-bar.js
+++ scripts/top-bar.js
@@ -1,12 +1,12 @@
 let topBar = (function () {
     let topBarPath = $$('#top-bar').children[0];
-    let logoutUser = $$('#top-bar-logout-user');
+    let logoutUser = $$('#top-bar-logout');
     let logoutDropdown = $$('#top-bar-logout-dropdown-menu');
     let userProfile = $$('#user-profile');
     let previousTopBar;
 
     if (sessionStorage.token) {
-        logoutUser.children[0].innerHTML = decodeToken(sessionStorage.token).preferred_username;
+        $$('#top-bar-logout-user').children[0].innerHTML = decodeToken(sessionStorage.token).preferred_username;
     }
     function showTopBar(value) {
         if (value.category && value.casino) {
@@ -28,12 +28,12 @@ let topBar = (function () {
         $$('#black-area').classList.add('show');
         // userProfile.classList.toggle('hidden');
     }
-    logoutUser.addEventListener('click', function () {
-        trigger('opened-arrow', { div: logoutUser });
-        $$('#top-bar-logout').classList.toggle('logout-is-opened');
-        logoutDropdown.classList.toggle('hidden');
-        // showProfile();
-    });
+    // logoutUser.addEventListener('click', function () {
+    //     trigger('opened-arrow', { div: $$('#top-bar-logout-user') });
+    //     logoutUser.classList.toggle('logout-is-opened');
+    //     logoutDropdown.classList.toggle('hidden');
+    //     // showProfile();
+    // });
 
     on('show/app', function () {
         userProfile.classList.add('hidden');
