d2f5aa8bbbca760939cecb861d389aa1795cd950
anta988 <nikola.antic@triplecrown.rs>
2019-07-03 09:59:21 +0200

    jackpot: bind selecAll button

--- scripts/jackpot/jackpot-details.js
+++ scripts/jackpot/jackpot-details.js
@@ -5,4 +5,13 @@ const jackpotDetailsBar = (function () {
         jackpotDetailsBarWrapper.classList.add('collapse');
     });
 
+    //select all checkboxes in jackpot details
+    $$('#jackpot-edit-details-conditions-header').children[1].onclick = function () {
+        for (let checkbox of $$('#jackpot-edit-details-conditions-checkbox').children) {
+            if (!checkbox.classList.contains('not-clickable')) {
+                checkboxChangeState.checkboxIsChecked(checkbox.getElementsByClassName('form-checkbox')[0].children[0], true);
+            }
+        }
+    }
+
 })();
